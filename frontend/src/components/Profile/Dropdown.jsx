5f954be16e24a94ebfd2b1a503fb8acfb01142c7
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2024-02-10 11:07:44 +0900

    fixed menuRef

--- frontend/src/components/Profile/Dropdown.jsx
+++ frontend/src/components/Profile/Dropdown.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useState, Fragment, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../../slices/usersApiSlice';
+import { logout } from '../../slices/authSlice';
 
 import { IoMoonOutline } from 'react-icons/io5';
 import { FaRegUserCircle } from 'react-icons/fa';
@@ -9,27 +11,47 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { GoChevronDown } from 'react-icons/go';
 
 import './_dropdown.scss';
+import { set } from 'mongoose';
 
 export const Dropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { userInfo } = useSelector((state) => state.auth);
+
+  const menuRef = useRef();
+  window.addEventListener('click', (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLogoutMutation();
 
   const handleClick = () => {
     if (userInfo) {
       setIsOpen(!isOpen);
     }
   };
-  const handleBlur = () => {
-    setIsOpen(false);
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+      setIsOpen(false);
+    } catch (err) {
+      console.log(err.message);
+    }
   };
+
   return (
-    <div className={`dropdown ${isOpen ? 'open' : ''}`} onBlur={handleBlur}>
+    <div className={`dropdown ${isOpen ? 'open' : ''}`} ref={menuRef}>
       <button className="btn-user" onClick={handleClick}>
         {userInfo ? (
-          <>
+          <Fragment>
             {userInfo.name}
             <GoChevronDown />
-          </>
+          </Fragment>
         ) : (
           <Link to="/login" className="login-btn">
             Login
@@ -49,7 +71,7 @@ export const Dropdown = () => {
           <IoSettingsOutline />
           Settings
         </button>
-        <button className="btn-logout">
+        <button className="btn-logout" onClick={handleLogout}>
           <AiOutlineLogout />
           Logout
         </button>
